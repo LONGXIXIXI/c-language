d2b013d6f0d118a9d6b78da67c081edb34acc3d1
LONGXIXIXI <115448031+LONGXIXIXI@users.noreply.github.com>
2022-10-10 16:53:41 +0800

    Add files via upload

---
+++ switch sentence.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+/*{
+	int day;
+	scanf("%d",&day);        //switch判断（整形表达式）中的数值,转到case
+	switch(day)
+	{
+		case 1:              //case 决定入口，后面必须是整形常量表达式  
+		printf("星期一\n");  //break代表终止，跳出循环
+		break;                
+		case 2:
+		printf("星期二\n");
+		break;
+		case 3:
+		printf("星期三\n");
+		break;
+		case 4:
+			printf("星期四\n");
+			break;
+			case 5:
+			printf("星期五\n");
+			break;
+			case 6:
+			printf("星期六\n");
+			break;
+			case 7:
+			printf("星期天\n");
+			break;
+	}
+	return 0;
+}*/
+{
+	int day;
+	scanf("%d",&day);
+	if(day>=1 && day<5)
+	printf("工作日");
+	else
+	printf("休息日");
+	return 0;
+}
