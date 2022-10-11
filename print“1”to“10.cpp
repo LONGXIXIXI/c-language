92b000c0e3f8a1512ee6bb0c14477491029a6b04
LONGXIXIXI <115448031+LONGXIXIXI@users.noreply.github.com>
2022-10-11 14:45:44 +0800

    Add files via upload

--- print“1”to“10.cpp
+++ print“1”to“10.cpp
@@ -4,6 +4,9 @@ int main()
 	int a=1;
 	while(a<=10)
 	{
+		if(a==5)
+		break;                //break代表停止后期的所有循环，直接终止循环
+		//continue;           //continue代表终止本次循环，也就是本次循环中的continue后边的代码不在执行，而是直接跳转到while语句的判断部分，进行下一次循环的入口判断
 		printf("%d ",a);
 		a++;
 		
